3bdac59320613a029d28d123115a8fdcca79bc65
Dermot Buckley <dermot@buckley.ie>
2013-04-20 13:31:26 -0300

    Whitespace changes

--- sickbeard/providers/kat.py
+++ sickbeard/providers/kat.py
@@ -38,7 +38,7 @@ class KATProvider(generic.TorrentProvider):
     def __init__(self):
 
         generic.TorrentProvider.__init__(self, "KAT")
-
+        
         self.supportsBacklog = True
 
         self.cache = KATCache(self)
@@ -47,58 +47,58 @@ class KATProvider(generic.TorrentProvider):
 
     def isEnabled(self):
         return sickbeard.KAT
-
+        
     def imageName(self):
         return 'kat.png'
-
+      
     def getQuality(self, item):
-
+        
         #torrent_node = item.getElementsByTagName('torrent')[0]
         #filename_node = torrent_node.getElementsByTagName('title')[0]
         #filename = get_xml_text(filename_node)
-
+        
         # I think the only place we can get anything resembing the filename is in 
         # the title
         filename = helpers.get_xml_text(item.getElementsByTagName('title')[0])
 
         quality = Quality.nameQuality(filename)
-
+        
         return quality
 
     def findSeasonResults(self, show, season):
-
+        
         results = {}
-
+        
         if show.air_by_date:
             logger.log(u"KAT doesn't support air-by-date backlog because of limitations on their RSS search.", logger.WARNING)
             return results
-
+        
         results = generic.TorrentProvider.findSeasonResults(self, show, season)
-
+        
         return results
     def _get_season_search_strings(self, show, season=None):
-
+    
         params = {}
-
+    
         if not show:
             return params
-
+        
         params['show_name'] = helpers.sanitizeSceneName(show.name).replace('.',' ').encode('utf-8')
-
+          
         if season != None:
             params['season'] = season
-
+    
         return [params]
 
     def _get_episode_search_strings(self, ep_obj):
-
+    
         params = {}
-
+        
         if not ep_obj:
             return [params]
 
         params['show_name'] = helpers.sanitizeSceneName(ep_obj.show.name).replace('.',' ').encode('utf-8')
-
+        
         if ep_obj.show.air_by_date:
             params['date'] = str(ep_obj.airdate)
         else:
@@ -138,7 +138,7 @@ class KATProvider(generic.TorrentProvider):
             usock = opener.open(url)
             url = usock.geturl()
             encoding = usock.info().get("Content-Encoding")
-
+    
             if encoding in ('gzip', 'x-gzip', 'deflate'):
                 content = usock.read()
                 if encoding == 'deflate':
@@ -146,19 +146,19 @@ class KATProvider(generic.TorrentProvider):
                 else:
                     data = gzip.GzipFile(fileobj=StringIO.StringIO(content))
                 result = data.read()
-
+    
             else:
                 result = usock.read()
-
+    
             usock.close()
-
+            
             return result
-
+    
         except urllib2.HTTPError, e:
             if e.code == 404:
                 # for a 404, we fake an empty result
                 return '<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel></channel></rss>'
-
+            
             logger.log(u"HTTP error " + str(e.code) + " while loading URL " + url, logger.ERROR)
             return None
         except urllib2.URLError, e:
@@ -288,7 +288,7 @@ class KATProvider(generic.TorrentProvider):
         if match:
             return match.group(1)
         return None
-
+    
 #    <item>
 #        <title>James Mays Things You Need To Know S02E06 HDTV XviD-AFG</title>
 #        <description>random text in here</description>        
@@ -333,7 +333,7 @@ class KATCache(tvcache.TVCache):
         if not title or not url:
             logger.log(u"The XML returned from the KAT RSS feed is incomplete, this result is unusable", logger.ERROR)
             return
-
+        
 #        if url and url.startswith('magnet:'):
 #            torrent_url = self.provider.magnetToTorrent(url)
 #            if torrent_url:
@@ -342,7 +342,7 @@ class KATCache(tvcache.TVCache):
 #            else:
 #                logger.log(u"Failed to handle magnet url %s, skipping..." % url, logger.DEBUG)
 #                return
-
+            
         if url and self.provider.urlIsBlacklisted(url):
             logger.log(u"url %s is blacklisted, skipping..." % url, logger.DEBUG)
             return
