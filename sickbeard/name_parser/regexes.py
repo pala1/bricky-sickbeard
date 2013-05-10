97641220d8979b59e80cec7d23e75d057cb62b7d
Dermot Buckley <dermot@buckley.ie>
2013-05-10 14:05:23 -0300

    more tweaks to site_at_start regex

--- sickbeard/name_parser/regexes.py
+++ sickbeard/name_parser/regexes.py
@@ -58,7 +58,7 @@ ep_regexes = [
                # [ www.Torrenting.com ] - Men.at.Work.S02E06.HDTV.XviD-AFG
                # [kat.ph]666.park.avenue.s01e12.vostfr.hdtv.xvid.itoma
                '''
-               ^(\[.+\][ -]*)?                             # likely a web address, surrounded by [ and ]
+               ^(\[.+\][ -]*)                              # likely a web address, surrounded by [ and ]
                ((?P<series_name>.+?)[. _-]+)?              # Show_Name and separator
                s(?P<season_num>\d+)[. _-]*                 # S01 and optional separator
                e(?P<ep_num>\d+)                            # E02 and separator
